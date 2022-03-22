56fe5230a565b97485e7f9f25201b7cbc97ebe88
arsatapathy <ashish.satapathy2014@gmail.com>
2022-03-22 08:40:31 +0530

    EvenFirst

---
+++ src/main/java/com/arsatapathy/array/EvenFirst.java
@@ -0,0 +1,20 @@
+package com.arsatapathy.array;
+
+public class EvenFirst {
+    public static int[] evenFrist(int[] array) {
+        int even = 0;
+        int odd = array.length - 1;
+
+        while (even <= odd) {
+            if (array[even] %2 == 0)
+                even ++;
+            else {
+                int temp = array[even];
+                array[even] = array[odd];
+                array[odd--] = temp;
+            }
+        }
+
+        return array;
+    }
+}
---
+++ src/test/java/com/arsatapathy/array/EvenFirstTest.java
@@ -0,0 +1,12 @@
+package com.arsatapathy.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EvenFirstTest {
+
+    @Test
+    public void test_01() {
+        Assertions.assertArrayEquals(new int[]{6,2,4,5,3,1}, EvenFirst.evenFrist(new int[]{1,2,3,4,5,6}));
+    }
+}
